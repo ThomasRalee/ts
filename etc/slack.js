17faf3175dc874c49566054d596660de2a9ab56d
thomasRalee <thomas.leera@gmail.com>
2023-05-02 22:44:18 +0800

    chore: minor

--- etc/slack.js
+++ etc/slack.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
+const { exec } = require('child_process')
 
 const main = () => {
   console.log('reading lerna packages!')
+
+  exec('ls', (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
+    if (error !== null) {
+      console.log(`exec error: ${error}`)
+    }
+  })
 
   fs.readFile(
     './lerna-publish-summary.json',
